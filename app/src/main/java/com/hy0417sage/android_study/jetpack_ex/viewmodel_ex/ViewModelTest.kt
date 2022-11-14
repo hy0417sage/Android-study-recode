02223ad80097c0368d929ba852001b43c293bdbc
Song HyunYoung <97173983+hy0417sage@users.noreply.github.com>
2022-11-14 16:35:15 +0900

    study) viewModel Factory

--- app/src/main/java/com/hy0417sage/android_study/jetpack_ex/viewmodel_ex/ViewModelTest.kt
+++ app/src/main/java/com/hy0417sage/android_study/jetpack_ex/viewmodel_ex/ViewModelTest.kt
@@ -3,12 +3,12 @@ package com.hy0417sage.android_study.jetpack_ex.viewmodel_ex
 import android.util.Log
 import androidx.lifecycle.ViewModel
 
-class ViewModelTest : ViewModel() {
+class ViewModelTest(num: Int) : ViewModel() {
 
     private val TAG = "ViewModelTest"
 
     init{
-        Log.d(TAG, "ViewModelTest init (생성)")
+        Log.d(TAG, num.toString())
     }
 
     //ViewModel 은 LiveData 와 함께 사용
